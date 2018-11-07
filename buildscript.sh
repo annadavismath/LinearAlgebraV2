552d31a0fbb8c11e8b37fa39ef7cda91215e7934
Paul Bender <paul.bender@acm.org>
2018-11-07 01:41:49 -0500

    for scripted build, don't attempt to edit the message.

--- buildscript.sh
+++ buildscript.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 # build script that uploads to ximera
 
+export GIT_MERGE_AUTOEDIT no
+
 # start by making sure ximera is up to date.
 # assumes !/texmf/tex/latex/ximeraLatex has a clone of ximeraLatex
 (cd ~/texmf/tex/latex/ximeraLatex ; git pull https://github.com/XimeraProject/ximeraLatex.git master )
 
 #make sure the content is up to date with the master repository
-git pull https://github.com/OhioOER/LinearAlgebra.git master
+git pull --no-edit https://github.com/OhioOER/LinearAlgebra.git master
 
 #setup the content.  We assume here that a bearer 
 # token already exists and is properly configured.
